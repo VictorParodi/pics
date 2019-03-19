93bd9917203a9602038b90f1dc22488c45bca159
VictorParodi <victor.parodi.montero@gmail.com>
2019-03-18 22:44:44 -0500

    app1 commit 4: adding floating button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => print('Button clicked'),
+      ),
+
       appBar: AppBar(
         title:Text('Lets see images !!!'),
       ),
